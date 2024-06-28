df24fb432682a504811742ca781e3b26581f8292
dilipsasdk <dilip.sharma@sas.dk>
2024-06-28 14:42:36 +0530

    test

--- app.js
+++ app.js
@@ -1,11 +1,43 @@
-const express = require('express'); 
-const app = express(); 
-const port = process.env.PORT || 3000; 
+'use strict';
+// Module Dependencies
+// -------------------
+var express     = require('express');
+var bodyParser  = require('body-parser');
+var errorhandler = require('errorhandler');
+var http        = require('http');
+var path        = require('path');
+var request     = require('request');
+var routes      = require('./routes');
+var activity    = require('./routes/activity');
 
-app.get('/', (req, res) => { 
-res.send('Hello, Azure! This is a Node.js application test 1.'); 
-}); 
+var app = express();
 
-app.listen(port, () => { 
-console.log(`Server is running on port ${port}`); 
-}); 
+// Configure Express
+app.set('port', process.env.PORT || 3000);
+app.use(bodyParser.raw({type: 'application/jwt'}));
+//app.use(bodyParser.urlencoded({ extended: true }));
+
+//app.use(express.methodOverride());
+//app.use(express.favicon());
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Express in Development Mode
+if ('development' == app.get('env')) {
+  app.use(errorhandler());
+}
+
+// HubExchange Routes
+app.get('/', routes.index );
+app.post('/login', routes.login );
+app.post('/logout', routes.logout );
+
+// Custom Hello World Activity Routes
+app.post('/journeybuilder/save/', activity.save );
+app.post('/journeybuilder/validate/', activity.validate );
+app.post('/journeybuilder/publish/', activity.publish );
+app.post('/journeybuilder/execute/', activity.execute );
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});
